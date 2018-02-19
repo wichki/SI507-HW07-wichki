ea04ff71b61d78ded897c94ac2ab28100c1716db
William Kim <wichki@umich.edu>
2018-02-19 13:15:32 -0500

    Cloned files.

---
+++ hw7_ec1.py
@@ -0,0 +1,6 @@
+# 507/206 Homework 7 Extra 1
+from bs4 import BeautifulSoup
+import csv
+
+#### Your Extra 1 solution goes here ####
+url = "https://www.freep.com/news/"
---
+++ hw7_ec2.py
@@ -0,0 +1,6 @@
+# 507/206 Homework 7 Extra 2
+from bs4 import BeautifulSoup
+
+#### Your Extra 2 solution goes here ####
+url = "https://a2gov.legistar.com/Departments.aspx"
+pre_url = "https://a2gov.legistar.com/"
